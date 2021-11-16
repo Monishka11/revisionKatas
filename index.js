00c2c55a889a8e7c88f74a435e4a2ad2d5c0d885
Monishka <monishkagohil1194@gmail.com>
2021-11-16 12:15:38 +0100

    id

--- index.js
+++ index.js
@@ -2,6 +2,33 @@
 //  Comment out the code which you do not wish to execute altogether
 
 // Multiply
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+
 multiply = (a, b) => a * b;
 
-// 
+// Get planet name by id
+// https://www.codewars.com/kata/515e188a311df01cba000003
+
+function getPlanetName(id){  
+    switch(id){
+      case 1:
+        return "Mercury";
+      case 2:
+        return "Venus";
+      case 3:
+        return "Earth";
+      case 4:
+        return "Mars";
+      case 5:
+        return "Jupiter";
+      case 6:
+        return "Saturn";
+      case 7:
+        return "Uranus";
+      case 8:
+        return "Neptune";
+      default:
+        return "invalid planet id in function getPlanetName()";
+    }
+}
+
